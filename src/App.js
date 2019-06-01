812d125951d7db8566e8436600a5ead50d178e18
Julian Krispel-Samsel <julian@reactrocket.com>
2019-06-01 12:54:13 +0100

    customize rendering

--- src/App.js
+++ src/App.js
@@ -3,12 +3,23 @@ import './App.css'
 import { Editor } from 'slate-react'
 import Plain from 'slate-plain-serializer'
 
-const initialValue = Plain.deserialize('Hello World')
+const initialValue = Plain.deserialize('One\nTwo\nThree')
 
 function App() {
   const [value, setValue] = useState(initialValue)
   return <div className="App">
     <Editor
+      renderBlock={(props, editor, next) => {
+        return <li>{props.children}</li>
+      }}
+      renderDocument={(props) => {
+        return <main>
+          <h1 contentEditable={false}>My sweet document</h1>
+          <ul>
+            {props.children}
+          </ul>
+        </main>
+      }}
       value={value}
       onChange={({ value }) => setValue(value)}
     />
