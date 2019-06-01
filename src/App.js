f5c448e1e5b6f280e574fe36d126c1e0f2ae1fc2
Julian Krispel-Samsel <julian@reactrocket.com>
2019-06-01 14:07:40 +0100

    move props into plugin

--- src/App.js
+++ src/App.js
@@ -5,26 +5,48 @@ import Plain from 'slate-plain-serializer'
 
 const initialValue = Plain.deserialize('Press cmd + b to toggle bold')
 
+const boldPlugin = {
+  onKeyDown: (event, editor, next) => {
+    // has the user pressed cmd + b
+    if (event.key === 'b' && event.metaKey) {
+      return editor.toggleMark('bold')
+    }
+
+    return next()
+  },
+  renderMark: (props, editor, next) => {
+    if (props.mark.type === 'bold') {
+      return <strong>{props.children}</strong>
+    }
+
+    return next()
+  }
+}
+
+const italicPlugin = {
+  onKeyDown: (event, editor, next) => {
+    // has the user pressed cmd + i
+    if (event.key === 'i' && event.metaKey) {
+      return editor.toggleMark('italic')
+    }
+
+    return next()
+  },
+  renderMark: (props, editor, next) => {
+    if (props.mark.type === 'italic') {
+      return <i>{props.children}</i>
+    }
+
+    return next()
+  }
+}
+
 function App() {
   const [value, setValue] = useState(initialValue)
   return <div className="App">
     <Editor
       value={value}
-      onKeyDown={(event, editor, next) => {
-        // has the user pressed cmd + b
-        if (event.key === 'b' && event.metaKey) {
-          return editor.toggleMark('bold')
-        }
-
-        return next()
-      }}
-      renderMark={(props, editor, next) => {
-        if (props.mark.type === 'bold') {
-          return <strong>{props.children}</strong>
-        }
-
-        return next()
-      }}
+      plugins={[ boldPlugin, italicPlugin ]}
       onChange={({ value }) => setValue(value)}
     />
   </div>
